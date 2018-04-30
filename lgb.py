7002d6876a84e9a55377936ec76bf4b16f469fc0
Peter Hurford <peter@peterhurford.com>
2018-04-29 20:27:51 -0500

    Bring the .225 back

--- lgb.py
+++ lgb.py
@@ -12,7 +12,7 @@ from sklearn.feature_selection.univariate_selection import SelectKBest, f_regres
 import lightgbm as lgb
 
 from cv import run_cv_model
-from utils import print_step, rmse, univariate_analysis, TargetEncoder
+from utils import print_step, rmse
 from cache import get_data, is_in_cache, load_cache, save_in_cache
 
 
@@ -56,38 +56,33 @@ test_id = test['item_id']
 train.drop(['deal_probability', 'item_id'], axis=1, inplace=True)
 test.drop(['item_id'], axis=1, inplace=True)
 
-
 if not is_in_cache('data_with_fe'):
     print('~~~~~~~~~~~~')
     print_step('Merging')
     merge = pd.concat([train, test])
 
     print('~~~~~~~~~~~~~~~')
-    print_step('Impute 1/7')
-    #merge['adjusted_item_seq_number'] = merge['item_seq_number'] - merge.groupby('user_id')['item_seq_number'].transform('min')
-    #merge['num_missing'] = merge.isna().sum(axis=1)
-    print_step('Impute 2/7')
+    print_step('Impute 1/6')
     merge['param_1_missing'] = merge['param_1'].isna().astype(int)
     merge['param_1'].fillna('missing', inplace=True)
-    print_step('Impute 3/7')
+    print_step('Impute 2/6')
     merge['param_2_missing'] = merge['param_2'].isna().astype(int)
     merge['param_2'].fillna('missing', inplace=True)
-    print_step('Impute 4/7')
+    print_step('Impute 3/6')
     merge['param_3_missing'] = merge['param_3'].isna().astype(int)
     merge['param_3'].fillna('missing', inplace=True)
-    print_step('Impute 5/7')
+    print_step('Impute 4/6')
     merge['price_missing'] = merge['price'].isna().astype(int)
     merge['price'].fillna(merge['price'].median(), inplace=True)
     merge['has_price'] = (merge['price'] > 0).astype(int)
-    print_step('Impute 6/7')
+    print_step('Impute 5/6')
     merge['description_missing'] = merge['description'].isna().astype(int)
     merge['description'].fillna('', inplace=True)
-    print_step('Impute 7/7')
-    merge['image_top_1'] = train['image_top_1'].fillna(-8).astype(str)
-
+    print_step('Impute 6/6')
+    merge['image_missing'] = merge['image'].isna().astype(int)
+    merge['image_top_1'] = merge['image_top_1'].astype('str').fillna('missing')
 
     print('~~~~~~~~~~~~~~~~~~~')
-    # Note: Many candidate features were considered, only features with univariate score >= 0.55 were kept.
     print_step('Basic NLP 1/32')
     merge['num_words_description'] = merge['description'].apply(lambda x: len(str(x).split()))
     print_step('Basic NLP 2/32')
@@ -172,9 +167,8 @@ if not is_in_cache('data_with_fe'):
     print_step('Dropping')
     drops = ['activation_date', 'title', 'description']
     merge.drop(drops, axis=1, inplace=True)
-    currently_unused = ['image']
+    currently_unused = ['user_id', 'image']
     merge.drop(currently_unused, axis=1, inplace=True)
-    print(merge.shape)
 
     print('~~~~~~~~~~~~')
     print_step('Unmerge')
@@ -191,7 +185,6 @@ else:
     print('~~~~~~~~~~~~~~~~~~')
     print_step('Cache Loading')
     train_fe, test_fe = load_cache('data_with_fe')
-
 
 
 print('~~~~~~~~~~~~~~~~')
@@ -228,12 +221,26 @@ merge.drop('text', axis=1, inplace=True)
 print(tfidf_train.shape)
 print(tfidf_test.shape)
 
+
+print('~~~~~~~~~~~~~~~~~')
+print('Pre-flight checks')
+merge['image_top_1'] = merge['image_top_1'].astype('str').fillna('missing')
+print('-')
 print(merge.shape)
-print('~~~~~~~~~~~~~')
-print_step('Dropping')
-drops = ['user_id']
-merge.drop(drops, axis=1, inplace=True)
-print(merge.shape)
+print('-')
+for col in merge.columns:
+	print('##')
+	print(col)
+	print('-')
+	print(merge[col].values)
+	print('-')
+	print(merge[col].value_counts())
+	print('-')
+	print(merge[col].dtype)
+	print(merge[col].isna().sum())
+	print(merge[col].nunique())
+	print('-')
+print('-')
 
 print('~~~~~~~~~~~~~~~~')
 print_step('Dummies 1/2')
@@ -278,66 +285,70 @@ print_step('Prepping submission file')
 submission = pd.DataFrame()
 submission['item_id'] = test_id
 submission['deal_probability'] = results['test'].clip(0.0, 1.0)
-submission.to_csv('submit/submit_lgb1.csv', index=False)
+submission.to_csv('submit/submit_lgb.csv', index=False)
 print_step('Done!')
 
-# LGB: no text, geo, date, image, param data, or item_seq_number                   - Dim 51,    5CV 0.2313, Submit 0.235, Delta -.0037
-# LGB: +missing data, +OHE params (no text, geo, date, image, or item_seq_number)  - Dim 5057,  5CV 0.2269, Submit 0.230, Delta -.0031
-# LGB: +basic NLP (no other text, geo, date, image, or item_seq_number)            - Dim 5078,  5CV 0.2261, Submit 0.229, Delta -.0029  <a9e424c>
-# LGB: +date (no other text, geo, image, or item_seq_number)                       - Dim 5086,  5CV 0.2261, Submit ?                    <f6c28f2>
-# LGB: +OHE city and region (no other text, image, or item_seq_number)             - Dim 6866,  5CV 0.2254, Submit ?                    <531df17>
-# LGB: +item_seq_number (no other text or image)                                   - Dim 6867,  5CV 0.2252, Submit ?                    <624f1a4>
-# LGB: +more basic NLP (no other text or image)                                    - Dim 6877,  5CV 0.2251, Submit 0.229, Delta -.0039  <f47d17d>
-# LGB: +SelectKBest TFIDF description + text (no image)                            - Dim 54877, 5CV 0.2221, Submit 0.225, Delta -.0029  <5e4f5be>
-# LGB: +SelectKBest TFIDF description + text (no image)                            - Dim 54875, 5CV 0.2220, Submit 0.225, Delta -.0029  <5e4f5be>
+# LOG (Comp start 25 Apr, merge deadline 20 June @ 7pm EDT, end 27 June @ 7pm EDT) (17/25 submits used as of 30 Apr)
+# LGB: no text, geo, date, image, param data, or item_seq_number                   - Dim 51,    5CV 0.2313, Submit 0.235, Delta -.00371
+# LGB: +missing data, +OHE params (no text, geo, date, image, or item_seq_number)  - Dim 5057,  5CV 0.2269, Submit 0.230, Delta -.00306
+# LGB: +basic NLP (no other text, geo, date, image, or item_seq_number)            - Dim 5078,  5CV 0.2261, Submit 0.229, Delta -.00293  <a9e424c>
+# LGB: +date (no other text, geo, image, or item_seq_number)                       - Dim 5086,  5CV 0.2261, Submit ?                     <f6c28f2>
+# LGB: +OHE city and region (no other text, image, or item_seq_number)             - Dim 6866,  5CV 0.2254, Submit ?                     <531df17>
+# LGB: +item_seq_number (no other text or image)                                   - Dim 6867,  5CV 0.2252, Submit ?                     <624f1a4>
+# LGB: +more basic NLP (no other text or image)                                    - Dim 6877,  5CV 0.2251, Submit 0.229, Delta -.00390  <f47d17d>
+# LGB: +SelectKBest TFIDF description + text (no image)                            - Dim 54877, 5CV 0.2221, Submit 0.225, Delta -.00290  <5e4f5be>
 # LGB: +                                                                           - Dim ?, 5CV ?, Submit ?
 
-# [2018-04-29 01:33:18.215149] lgb cv scores : [0.22254376970558482, 0.22157821504665512, 0.22189080428582306, 0.2218044691982475, 0.222405863025811]
-# [2018-04-29 01:33:18.218096] lgb mean cv score : 0.22204462425242433
-# [2018-04-29 01:33:18.220445] lgb std cv score : 0.0003683781248138519
+# CURRENT
+# [2018-04-26 21:46:32.974844] lgb cv scores : [0.22256053295460215, 0.22160517248374503, 0.22192232562406788, 0.22177930697296735, 0.22242885702632134]
+# [2018-04-26 21:46:32.976301] lgb mean cv score : 0.22205923901234076
+# [2018-04-26 21:46:32.979111] lgb std cv score : 0.00037180552114082565
+
 
 
 # TODO
-# Total missing
-# Look for covariate shifts
-# Add SVD of text
-# Add Embedding and start doing embedding corrections
-    # https://github.com/nlpub/russe-evaluation/tree/master/russe/measures/word2vec
-# See if SVD + embedding + top 300 words
+# Are params subcategories? Are they entirely missing for some categories? Are they partially missing in any categories? Fix imputation?
 # Do image and image_top_1 match?
-# Are params subcategories? are they entirely missing for some categories? Are they partially missing in any categories?
 # Compare adjusted sequence number to user_id ordered by date
 # Are there users in multiple regions? Multiple cities?
 # Recategorize categories according to english translation (maybe by hand or CountVectorizer)
 # Are there users in multiple parent categories? Regular categories? Recategorized categories?
-# User attempt by category (group by category / user, order by date, number)
-# Population encode region?
-# Category - region interaction?
-# Look at user_ids that are in both train and test (careful!)
-# Delta between price and price of category (careful!)
 
-
-
-# Price encoding?
-
-# Frequency and price encode parent_category_name X region, parent_category_name X city
-
-# Target encoding? (careful!)
-
-# Price minus mean price for category
-# Price divided by mean price for category
-
+# Total missing
 # Mean and max length of word
 # Inclusion of numerics
 # Include more punctuation in punctuation
+# if any lazy people used the same or similar entries for the title/description fields
 
 # Handle time features
-	# https://github.com/mxbi/ftim
-	# Try OTV validation
+       # https://github.com/mxbi/ftim
+       # Try OTV validation
     # Include date as a feature
     # Days since user last posted and such
 
+# Look for covariate shifts
+
+# Fix categorical encoding (frequency encode / LGB encode / one-hot encode / SelectKBest encode / SVD encode / Ridge encode) (careful!)
+	# https://www.kaggle.com/peterhurford/beep-beep-2-lgb-encode/edit
+	# https://www.kaggle.com/peterhurford/beep-beep-2
+	# https://www.kaggle.com/the1owl/beep-beep?scriptVersionId=3404599
+	# Predict log price to impute missing, also look at difference between predicted and actual price
+# Population encode region/city? (careful!)
+# Geo encode region/city? (careful!)
+# Fix target encoding (ridge encode / target mean-encode / target KFold-mean encode) (careful!)
+	# https://www.kaggle.com/peterhurford/ridge-encoding
+	# https://www.kaggle.com/tnarik/likelihood-encoding-of-categorical-features
+# Handle price outliers
+# Encode mean price by category (careful!)
+# User attempt by category (group by category / user, order by date, number) (careful!)
+
 # Handle russian inflectional structure <https://www.kaggle.com/iggisv9t/handling-russian-language-inflectional-structure>
+# Russian NLP http://www.redhenlab.org/home/the-cognitive-core-research-topics-in-red-hen/the-barnyard/russian-nlp
+
+# Try Title SVD + Desc SVD vs. Text SVD vs. Title SVD + Desc SVD + Text SVD
+# Add Embedding and start doing embedding corrections
+    # https://github.com/nlpub/russe-evaluation/tree/master/russe/measures/word2vec
+# See if SVD + embedding + top 300 words
 
 #get_element = lambda elem, item: elem[item] if len(elem) > item else ''
 #tr['title_first'] = tr['title'].apply(lambda ss: get_element(ss.translate(ss.maketrans('', '', string.punctuation)).replace('\n', ' ').lower().split(' '), 0))
@@ -347,39 +358,51 @@ print_step('Done!')
 #tr['title_last'] = tr['title'].apply(lambda ss: get_element(ss.translate(ss.maketrans('', '', string.punctuation)).replace('\n', ' ').lower().split(' '), -1))
 # 0.7815870814266627
 
-# Try Title SVD + Desc SVD vs. Text SVD vs. Title SVD + Desc SVD + Text SVD
+# Understand and apply https://www.kaggle.com/rdizzl3/stage-2-lgbm-stacker-8th-place-solution/code
+
+# Category - region interaction?
+# Look at user_ids that are in both train and test (careful!)
+# Delta between price and price of category (careful!)
 
 # Translate to english?
 # Words in other words (e.g., param_1 or title in descripton)?
 
 # Image analysis
+	# https://www.kaggle.com/wesamelshamy/image-classification-and-quality-score-w-resnet50
     # pic2vec
     # img_hash.py?
-	# https://www.kaggle.com/classtag/extract-avito-image-features-via-keras-vgg16
+    # https://www.kaggle.com/classtag/extract-avito-image-features-via-keras-vgg16
     # Contrast? https://dsp.stackexchange.com/questions/3309/measuring-the-contrast-of-an-image
     # https://www.pyimagesearch.com/2014/03/03/charizard-explains-describe-quantify-image-using-feature-vectors/
     # NNs?
-# Geo stuff with the cities (get lat/long)
+
 # Train classification model with AUC
-# Implement https://www.kaggle.com/yekenot/ridge-text-cat, add as submodel
+
+# Owe two novel contributions in kernels (pay it forward for images, Russian NLP)
+
 # Check feature impact and tuning in DR
 # Tune models some
 # Vary model
     # Train Ridge on text, include into as-is LGB
     # Take LGB, add text with Ridge / SelectKBest
     # Take LGB, add text with SVD + embedding
+    # Take LGB, add text OHE with Ridge / SelectKBest
+    # Take LGB, add text OHE with SVD + embedding
     # OHE everything into Ridge and then take just encoded categorical and numeric into LGB and boost with LGB
     # OHE everything into LGB except text, then use text and residuals and boost with Ridge
 # Train more models (Ridge, FM, Ridge, NNs)
-# Predict log price to impute missing, also look at difference between predicted and actual price
+
 # Look to DonorsChoose
     # https://www.kaggle.com/qinhui1999/deep-learning-is-all-you-need-lb-0-80x/code
     # https://www.kaggle.com/fizzbuzz/the-all-in-one-model
-    # https://www.kaggle.com/emotionevil/beginners-workflow-meanencoding-lgb-nn-ensemble
-    # https://www.kaggle.com/safavieh/ultimate-feature-engineering-xgb-lgb-nn
     # https://www.kaggle.com/jagangupta/understanding-approval-donorschoose-eda-fe-eli5
     # https://www.kaggle.com/fizzbuzz/beginner-s-guide-to-capsule-networks
     # https://www.kaggle.com/nicapotato/abc-s-of-tf-idf-boosting-0-798
-# Russian NLP http://www.redhenlab.org/home/the-cognitive-core-research-topics-in-red-hen/the-barnyard/russian-nlp
+	# https://www.kaggle.com/shadowwarrior/1st-place-solution
+
+# https://www.kaggle.com/c/avito-duplicate-ads-detection/discussion
+# https://www.kaggle.com/c/two-sigma-connect-rental-listing-inquiries/discussion
+# https://www.kaggle.com/c/cdiscount-image-classification-challenge/discussion
+
 # Use train_active and test_active somehow?
 # Denoising autoencoder? https://github.com/phdowling/mSDA
